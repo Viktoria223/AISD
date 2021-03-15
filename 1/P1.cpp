2cf867ea54e74c98a3053b96158fda368761d4f6
Fedorchuk_Viktoria <fedorchuk-vika03@mail.ru>
2021-03-15 20:24:19 +0300

    //

--- 1/P1.cpp
+++ 1/P1.cpp
@@ -3,73 +3,81 @@
 //
 
 #include <iostream>
-#include <list>
 
 using namespace std;
 
-/*int main() {
-    int n;
-    cin >> n;
-    list<int> list1;
-    list1.push_back(2);
-    int y = 1;
-    int j = 2;
-    while (y != n) {
-        j++;
-        bool f = true;
-        for (int i : list1) {
-            if (j % i == 0) {
-                f = false;
-            }
-        }
-        if (f) {
-            list1.push_back(j);
-            y++;
-        }
-    }
-    for (int i : list1) {
-        cout << i << " " << endl;
-    }
-    return 0;
-}*/
+struct elem {
+    int val;
+    elem *next;
+};
 
+void push(elem *&head, int n) {
+    elem *p = new elem;
+    p->val = n;
+    p->next = head;
+    head = p;
+}
+
+int pop(elem *&head) {
+    elem *p = head;
+    head = head->next;
+    int n = p->val;
+    delete p;
+    return n;
+}
+
+bool empty(elem *head) {
+    return head == NULL;
+}
+
+void print_list(elem* head) {
+    elem* p = head;
+    if (p) {
+        print_list(p -> next);
+        cout << p -> val << " ";
+    }
+}
+
+bool check_list(elem* head, int x) {
+    elem * p = head;
+    while (p) {
+        int a = p -> val;
+        if (x % a == 0) {
+            return false;
+        }
+        p = p -> next;
+    }
+    return true;
+}
 
 int main() {
     int n;
     cin >> n;
-    list<int> list;
-    list.push_back(2);
+    elem *head = NULL;
+    push(head, 2);
     int y = 1;
     int j = 2;
     if (n == 1) {
         cout << j << endl;
     } else {
         if (n == 2) {
-            list.push_back(3);
-            for (int i : list) {
-                cout << i << " " << endl;
-            }
+            push(head, 3);
+            print_list(head);
         } else {
-            list.push_back(3);
+            push(head, 3);
             y++;
             j = 3;
             while (y != n) {
                 j += 2;
                 bool f = true;
-                for (int i : list) {
-                    if (j % i == 0) {
-                        f = false;
-                        break;
-                    }
-                }
+                check_list(head, j);
+                //}
                 if (f) {
-                    list.push_back(j);
+                    push(head, j);
                     y++;
                 }
             }
-            for (int i : list) {
-                cout << i << " ";
-            }
+            print_list(head);
         }
     }
 }
